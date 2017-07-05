0159a871ff9a2a4b8eee1c7ef30361263c6f81c9
Brennan Baldridge <bbaldridge@moodyprice.com>
2017-07-05 16:23:16 -0500

    added updating script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server.');
+    }
+    console.log('Connected to MongoDB server.');
+
+    // findOneAndUpdate
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('595be0860a0fe5392cc46eab')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('595be1c2cfec953955589ea2')
+    }, {
+        $set: {
+            name: 'Brennan'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
