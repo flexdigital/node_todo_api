7631d24f0a928e74c6828b911c1fd3ed2c3d07ba
Brennan Baldridge <bbaldridge@moodyprice.com>
2017-07-05 20:22:36 -0500

    setup app for Heroku

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ var {User} = require('./models/user');
 
 // app
 var app = express();
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(bodyParser.json());
@@ -53,8 +54,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 // porting
-app.listen(3000, () => {
-    console.log('Started on Port 3000');
+app.listen(port, () => {
+    console.log(`Started on Port ${port}`);
 });
 
 module.exports = {app};
